179629a7988ceb82b863c9ed0f2ae23f413dd914
zessirb <zessirb@gmail.com>
2019-05-14 01:48:39 +0200

    Added invoke compatibility with default mob names

--- src/main/java/com/onaple/brawlator/actions/MonsterAction.java
+++ src/main/java/com/onaple/brawlator/actions/MonsterAction.java
@@ -13,27 +13,33 @@ import org.spongepowered.api.world.Location;
 import java.util.Optional;
 
 public class MonsterAction {
-    public Entity invokeMonster(Location location, String monsterName) throws EntityTypeNotFoundException, MonsterNotFoundException {
+    public void invokeMonster(Location location, String monsterName) throws EntityTypeNotFoundException, MonsterNotFoundException {
         Optional<MonsterBean> monsterOptional = ConfigurationHandler.getMonsterList().stream().filter(m -> m.getName().toLowerCase().equals(monsterName.toLowerCase())).findAny();
-        if (!monsterOptional.isPresent()) {
-            throw new MonsterNotFoundException(monsterName);
+        if (monsterOptional.isPresent()) {
+            MonsterBean monster = monsterOptional.get();
+
+            Optional<EntityType> entityTypeOptional = Sponge.getRegistry().getType(EntityType.class, monster.getType());
+            if (!entityTypeOptional.isPresent()) {
+                throw new EntityTypeNotFoundException(monster.getType());
+            }
+
+            Entity baseEntity = location.createEntity(entityTypeOptional.get());
+
+            baseEntity.offer(Keys.MAX_HEALTH, monster.getHp());
+            baseEntity.offer(Keys.HEALTH, monster.getHp());
+            baseEntity.offer(Keys.WALKING_SPEED, monster.getSpeed());
+            baseEntity.offer(Keys.ATTACK_DAMAGE, monster.getAttackDamage());
+            baseEntity.offer(Keys.KNOCKBACK_STRENGTH, monster.getKnockbackResistance());
+
+            location.spawnEntity(baseEntity);
+        } else {
+            Optional<EntityType> entityTypeOptional = Sponge.getRegistry().getType(EntityType.class, monsterName);
+            if (!entityTypeOptional.isPresent()) {
+                throw new MonsterNotFoundException(monsterName);
+            }
+
+            Entity baseEntity = location.createEntity(entityTypeOptional.get());
+            location.spawnEntity(baseEntity);
         }
-        MonsterBean monster = monsterOptional.get();
-
-        Optional<EntityType> entityTypeOptional = Sponge.getRegistry().getType(EntityType.class, monster.getType());
-        if (!entityTypeOptional.isPresent()) {
-            throw new EntityTypeNotFoundException(monster.getType());
-        }
-
-        Entity baseEntity = location.createEntity(entityTypeOptional.get());
-        baseEntity.offer(Keys.MAX_HEALTH, monster.getHp());
-        baseEntity.offer(Keys.HEALTH, monster.getHp());
-        baseEntity.offer(Keys.WALKING_SPEED, monster.getSpeed());
-        baseEntity.offer(Keys.ATTACK_DAMAGE, monster.getAttackDamage());
-        baseEntity.offer(Keys.KNOCKBACK_STRENGTH, monster.getKnockbackResistance());
-
-        location.spawnEntity(baseEntity);
-
-        return baseEntity;
     }
 }
